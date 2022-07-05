7eaf3df386205f2dc8bfd05ffbc79028524a2dc1
MahekChudesara <mahekchudesara12@gmail.com>
2022-07-05 16:00:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Statewise from "./Components/statewiswData/statewise";
+import './index.css'
+const App = () =>{
+  return(
+    <>
+      <Statewise/>
+    </>
+  )
+}
+export default App;
